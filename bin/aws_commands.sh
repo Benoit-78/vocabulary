3489ffb78e919c767142b2791b2e4d4668932a03
Benoit-78 <benoit.delorme@gadz.org>
2023-12-10 23:03:25 +0100

    ENH: created Route53 Domain name, waiting for DNS propagation

--- bin/aws_commands.sh
+++ bin/aws_commands.sh
@@ -9,8 +9,6 @@ aws configure
 # IAM
 # ====================
 # Policy
-
-
 # Roles
 aws iam attach-role-policy \
     --role-name vocabulary_read_on_bucket \
@@ -20,7 +18,6 @@ aws iam list-attached-role-policies \
     --role-name vocabulary_read_on_bucket
 
 # User
-
 
 # ====================
 # EC2
@@ -33,12 +30,12 @@ aws ec2 create-security-group \
     --description "Allows users to access the application" \
     --vpc-id vpc-0011e2cd73034beb9
 
-grep "GroupId" > conf/security_group.txt
+grep "GroupId"
 
-# Keep this on a single line
-aws ec2 authorize-security-group-ingress --group-id sg-08ebad99d1b1b98b6 --protocol tcp --port 8080 --cidr 0.0.0.0/0
-# Keep this on a single line
 aws ec2 authorize-security-group-ingress --group-id sg-08ebad99d1b1b98b6 --protocol tcp --port 22 --cidr 88.161.167.12/32
+aws ec2 authorize-security-group-ingress --group-id sg-08ebad99d1b1b98b6 --protocol tcp --port 8080 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id sg-08ebad99d1b1b98b6 --protocol tcp --port 80 --cidr 0.0.0.0/0
+
 
 aws ec2 describe-security-groups \
     --group-ids sg-08ebad99d1b1b98b6
@@ -65,58 +62,9 @@ aws ec2 create-key-pair \
     --query 'KeyMaterial' \
     --output text > voc_ssh_key_1.pem
 
-aws ec2 run-instances \
-    --image-id ami-00983e8a26e4c9bd9 \
-    --instance-type t2.medium \
-    --key-name voc_ssh_key_1 \
-    --security-group-ids sg-08ebad99d1b1b98b6 \
-    --block-device-mappings '[{"DeviceName":"/dev/sda1","Ebs":{"VolumeSize":20,"VolumeType":"gp2"}}]' \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=vocabulary,Value=compute}]' \
-    --iam-instance-profile Name=vocabulary_read_on_bucket \
-    --user-data file://bin/user_data.sh
-
-grep InstanceId > conf/instance_id.txt
-
-aws ec2 describe-instances \
-    --instance-ids i-01334f73020ce5513 \
-    --query "Reservations[0].Instances[0].State.Name"
-
-# Create Internet Gateway
-aws ec2 create-internet-gateway
-
-grep InternetGatewayId
-
-# Keep this on a single line
-aws ec2 attach-internet-gateway --internet-gateway-id igw-067bdaf3b90f544d8 --vpc-id vpc-0011e2cd73034beb9
-
-
-
-# Create an elastic IP
-aws ec2 allocate-address \
-    --domain vpc
-
-grep PublicIp > conf/public_ip.txt
-grep AllocationId > conf/allocation_id.txt
-
-aws ec2 associate-address \
-    --allocation-id eipalloc-07c0c9dd56ff7585a \
-    --instance-id i-0a76b8efc2f9842e3
-# Check the route table, and manually confirm the internet gateway
-# Check the Access Control List
-
-# Actions -> Monitor and troubleshoot -> Get system log
-# Why does an elastic IP address need an internet gateway, and a standard IP address does not?
-# How can I know if my instance is on a private or public network?
-
-# Use '-' in the IP address, not '.'
-ssh -i conf/voc_ssh_key_1.pem ubuntu@ec2-15-237-121-125.eu-west-3.compute.amazonaws.com
-
-
-
-# ====================
-# S3
-# ====================
-aws s3 sync /home/benoit/Documents/vocabulary s3://vocabulary-benito/vocabulary \
+aws s3 sync \
+    /home/benoit/Documents/vocabulary \
+    s3://vocabulary-benito/vocabulary \
     --exclude ".pytest_cache/*" \
     --exclude ".vscode/*" \
     --exclude "__pycache__/*" \
@@ -144,26 +92,74 @@ aws s3 sync /home/benoit/Documents/vocabulary s3://vocabulary-benito/vocabulary 
     --exclude "README.md" \
     --exclude "token.txt" \
     --exclude "*.exe" \
-    --exclude "*.pyc" \
     --exclude "*.pdf" \
+    --exclude "*.png" \
+    --exclude "*.pyc" \
     --exclude "*.txt"
 
-# Delete a folder
-aws s3 rm s3://vocabulary-benito/vocabulary/logs \
-    --recursive
+aws ec2 run-instances \
+    --image-id ami-00983e8a26e4c9bd9 \
+    --instance-type t2.medium \
+    --key-name voc_ssh_key_1 \
+    --security-group-ids sg-08ebad99d1b1b98b6 \
+    --block-device-mappings '[{"DeviceName":"/dev/sda1","Ebs":{"VolumeSize":20,"VolumeType":"gp2"}}]' \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=vocabulary,Value=compute}]' \
+    --iam-instance-profile Name=vocabulary_read_on_bucket \
+    --user-data file://bin/user_data.sh
 
-# Delete a file
-aws s3 rm s3://vocabulary-benito/vocabulary/bin/aws_commands.sh
+grep InstanceId
 
-# Download
-aws s3 cp s3://vocabulary-benito/vocabulary /home/ubuntu/vocabulary \
-    --recursive
+aws ec2 describe-instances \
+    --instance-ids i-063a9eca2e01c2575 \
+    --query "Reservations[0].Instances[0].State.Name"
+
+# Create Internet Gateway
+aws ec2 create-internet-gateway
+
+grep InternetGatewayId
+
+aws ec2 attach-internet-gateway --internet-gateway-id igw-067bdaf3b90f544d8 --vpc-id vpc-0011e2cd73034beb9
+
+# Create an elastic IP
+aws ec2 allocate-address \
+    --domain vpc
+
+grep PublicIp
+grep AllocationId
+
+aws ec2 associate-address \
+    --allocation-id eipalloc-03d7c5dae53ffbc4b \
+    --instance-id i-063a9eca2e01c2575
+# Check the route table, and manually confirm the internet gateway
+# Check the Access Control List
+# Actions -> Monitor and troubleshoot -> Get system log
+# Why does an elastic IP address need an internet gateway, and a standard IP address does not?
+# How can I know if my instance is on a private or public network?
+
+# Use '-' in the IP address, not '.'
+ssh -i conf/voc_ssh_key_1.pem ubuntu@ec2-35-181-220-245.eu-west-3.compute.amazonaws.com
 
 
 
 # =======================
 # Environment
 # =======================
+# Packages
+sudo apt-get update
+sudo apt-get install -y awscli
+sudo apt-get install -y default-libmysqlclient-dev
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+sudo apt-get install -y mariadb-client
+sudo apt-get install -y uvicorn
+sudo apt-get install -y nginx
+
+# Data
+aws s3 cp s3://vocabulary-benito/vocabulary /home/ubuntu/vocabulary \
+    --recursive
+
+# Python
 export PYTHONPATH=/home/ubuntu/vocabulary/src:$PYTHONPATH
 cd vocabulary
 python3 -m venv env
@@ -171,7 +167,27 @@ source env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Nginx
+sudo nano /etc/nginx/sites-available/vocabulary_app.com
+sudo ln -s /etc/nginx/sites-available/vocabulary_app.com /etc/nginx/sites-enabled/
+sudo service nginx restart
+
+
 uvicorn src.web_app:app --reload --port 8080 --host 0.0.0.0
+
+
+
+# =======================
+# Troubleshooting
+# =======================
+# Delete a folder
+aws s3 rm s3://vocabulary-benito/vocabulary/logs \
+    --recursive
+
+# Delete a file
+aws s3 rm s3://vocabulary-benito/vocabulary/bin/aws_commands.sh
+# See nginx error log
+sudo tail -f /var/log/nginx/error.log
 
 
 
--- bin/user_data.sh
+++ bin/user_data.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
-sudo apt-get install -y pkg-config
-sudo apt-get install -y default-libmysqlclient-dev
-sudo apt-get install -y python3-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python3.10-venv
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y mariadb-client
 sudo apt-get install -y awscli
+sudo apt-get install -y default-libmysqlclient-dev
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+sudo apt-get install -y mariadb-client
 sudo apt-get install -y uvicorn
+# sudo apt-get install -y pkg-config
+# sudo apt-get install -y libssl-dev
 # sudo apt-get install -y build-essential
